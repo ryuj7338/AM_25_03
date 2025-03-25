537ff2100c66c8d9a69222abfe6d4df74f10bf3b
ryuj7338 <ryuj7338@gmail.com>
2025-03-25 16:45:35 +0900

    ArticleManager 비밀번호 확인

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -154,18 +154,40 @@ public class Main {
                 String regDate = Util.getNowStr();
                 System.out.print("이름: ");
                 String name = sc.nextLine().trim();
-                System.out.print("아이디: ");
-                String loginId = sc.nextLine().trim();
 
+                String loginId = null;
 
+                while (true) {
+                    System.out.print("아이디: ");
+                    loginId = sc.nextLine().trim();
+                    if (joinableId(loginId) == Boolean.parseBoolean(loginId)) {
+                        System.out.println("이미 등록된 아이디입니다.");
+                        continue;
+                    }
+                    break;
+                }
                 System.out.println("아이디가 생성되었습니다.");
-                System.out.print("비밀번호: ");
-                String loginPw = sc.nextLine().trim();
+
+                String password = null;
+                String passwordCheck = null;
+
+                while (true) {
+                    System.out.print("비밀번호: ");
+                    password = sc.nextLine().trim();
+                    System.out.print("비밀번호 확인: ");
+                    passwordCheck = sc.nextLine().trim();
+                    if (password.equals(passwordCheck) == false) {
+                        System.out.println("비밀번호가 일치하지 않습니다.");
+                        continue;
+                    }
+                    break;
+                }
+
                 System.out.println("비밀번호가 설정되었습니다.");
 
-                Join join = new Join(id, regDate, loginId, loginPw, name);
+                Join join = new Join(id, regDate, loginId, password, name);
                 joins.add(join);
-                System.out.print("%d번 회원이 가입되었습니다.\n");
+                System.out.printf("%d번 회원이 가입되었습니다.\n", id);
                 lastMembersId++;
 
 
@@ -178,6 +200,15 @@ public class Main {
         }
 
 
+    }
+
+    private static boolean joinableId(String loginId) {
+        for (Join join : joins) {
+            if (join.getLoginId().equals(loginId)) {
+                return false;
+            }
+        }
+        return true;
     }
 
     private static Article getArticle(int id) {
